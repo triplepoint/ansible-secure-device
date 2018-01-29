38fdcb00e75f3f9796e34936073389eff18ca930
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-01-28 19:10:16 -0800

    Turns out I just needed pytest to test something

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,11 +1,16 @@
 import os
-# import pytest
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# def test_docker_service_enabled(host):
-#     service = host.service('docker')
-#     assert service.is_enabled
+@pytest.mark.parametrize('socket_def', [
+    # SSH port is open
+    ('tcp://22'),
+
+])
+def test_listening_sockets(host, socket_def):
+    socket = host.socket(socket_def)
+    assert socket.is_listening
