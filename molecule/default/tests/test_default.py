ef26af7938d9c3ddb41f882c702379a9c5d14a40
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-01-28 17:17:37 -0800

    Molecule implementation

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+# def test_docker_service_enabled(host):
+#     service = host.service('docker')
+#     assert service.is_enabled
