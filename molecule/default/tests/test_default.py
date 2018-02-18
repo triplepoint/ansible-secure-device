ebff33a1cacf630f0196435d966dc427648670d8
Jonathan Hanson <jonathan@jonathan-hanson.org>
2018-02-17 17:29:47 -0800

    Add support for opening ports for specific protocols, vs assuming "all" protocols in all cases.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,11 +7,30 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('socket_def', [
-    # SSH port is open
-    ('tcp://22'),
+# These tests are broken, because testinfra wants to test whether a port is
+# "listening", and can't test on whether a port is just open.
 
+@pytest.mark.parametrize('socket_def', [
+    # ('tcp://80'),
+    # ('udp://80'),
+    # ('tcp://100'),
+    # ('tcp://101'),
+    # ('tcp://102'),
+    # ('tcp://103'),
+    # ('udp://300'),
 ])
 def test_listening_sockets(host, socket_def):
     socket = host.socket(socket_def)
     assert socket.is_listening
+
+
+@pytest.mark.parametrize('socket_def', [
+    ('udp://100'),
+    ('udp://101'),
+    ('udp://102'),
+    ('udp://103'),
+    ('tcp://300'),
+])
+def test_closed_sockets(host, socket_def):
+    socket = host.socket(socket_def)
+    assert not socket.is_listening
